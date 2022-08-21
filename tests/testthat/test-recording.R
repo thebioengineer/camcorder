826b10b74de445aa68b138e2db2c0e55dfdcd06e
thebioengineer <ellishughes@live.com>
2022-08-21 10:54:28 -0700

    update and simplify tests

--- tests/testthat/test-recording.R
+++ tests/testthat/test-recording.R
@@ -3,7 +3,7 @@ test_that("recording a basic ggplot works", {
   rec_dir <- file.path(tempdir(),"camcorder_tests_ggplot")
 
   if(dir.exists(rec_dir)){
-    unlink(rec_dir)
+    unlink(rec_dir,recursive = TRUE)
   }
 
   gg_record(dir = rec_dir)
@@ -12,9 +12,7 @@ test_that("recording a basic ggplot works", {
   mtcars_plot <- ggplot2::ggplot(mtcars) +
     ggplot2::geom_boxplot(ggplot2::aes(x = factor(cyl), y = mpg))
 
-  expect_snapshot(
-    record_ggplot(mtcars_plot)
-  )
+  record_ggplot(mtcars_plot)
 
   ## Recording created directory
   expect_true(dir.exists(rec_dir))
@@ -23,8 +21,12 @@ test_that("recording a basic ggplot works", {
   expect_equal(length(list.files(rec_dir)), 1)
 
   ## preview film shows the file
-  expect_snapshot(
-    preview_film()
+  preview_html <-  preview_film()
+
+  file.rename(preview_html,file.path(tempdir(),"camcorder_preview_ggplot.html"))
+
+  expect_snapshot_file(
+    path = file.path(tempdir(),"camcorder_preview_ggplot.html")
   )
 
   expect_equal(
@@ -32,12 +34,86 @@ test_that("recording a basic ggplot works", {
     mtcars_plot
   )
 
+})
+
+test_that("recording a basic patchwork works", {
+
+  withr::with_package("patchwork",code = {
+
+  rec_dir <- file.path(tempdir(),"camcorder_tests_patchwork")
+
+  if(dir.exists(rec_dir)){
+    unlink(rec_dir,recursive = TRUE)
+  }
+
+  gg_record(dir = rec_dir)
+  on.exit(gg_stop_recording())
+
+  mtcars_plot <- ggplot2::ggplot(mtcars) +
+    ggplot2::geom_boxplot(ggplot2::aes(x = factor(cyl), y = mpg))
+
+  mtcars_patchwork <- wrap_plots(mtcars_plot,mtcars_plot,ncol = 2)
+
+  record_patchwork(mtcars_patchwork)
+
+  ## Recording created directory
+  expect_true(dir.exists(rec_dir))
+
+  ## Recording added a single file
+  expect_equal(length(list.files(rec_dir)), 1)
+
+  ## preview film shows the file
+  preview_html <-  preview_film()
+
+  file.rename(preview_html,file.path(tempdir(),"camcorder_preview_patchwork.html"))
+
+  expect_snapshot_file(
+    path = file.path(tempdir(),"camcorder_preview_patchwork.html")
+  )
+
+  expect_equal(
+    GG_RECORDING_ENV$last_plot,
+    mtcars_patchwork
+  )
+
+  })
+})
+
+test_that("recording a basic graphic devices works", {
+
+  rec_dir <- file.path(tempdir(),"camcorder_tests_polaroid")
+
+  if(dir.exists(rec_dir)){
+    unlink(rec_dir,recursive = TRUE)
+  }
+
+  gg_record(dir = rec_dir)
+  on.exit(gg_stop_recording())
+
+  plot(1:5)
+
+  record_polaroid()
+
+  ## Recording created directory
+  expect_true(dir.exists(rec_dir))
+
+  ## Recording added a single file
+  expect_equal(length(list.files(rec_dir)), 1)
+
+  ## preview film shows the file
+  preview_html <-  preview_film()
+
+  file.rename(preview_html,file.path(tempdir(),"camcorder_preview_polaroid.html"))
+
+  expect_snapshot_file(
+    path = file.path(tempdir(),"camcorder_preview_polaroid.html")
+  )
 
 })
 
-test_that("recording a basic ggplot works - gif output", {
+test_that("recording works - gif output", {
 
-  rec_dir <- file.path(tempdir(),"camcorder_tests_ggplot_2")
+  rec_dir <- file.path(tempdir(),"camcorder_tests_playback")
 
   if(dir.exists(rec_dir)){
     unlink(rec_dir)
@@ -54,20 +130,21 @@ test_that("recording a basic ggplot works - gif output", {
                   ggplot2::scale_x_discrete(labels = c("4 CYL","6 CYL","8 CYL"))
   )
 
-  expect_snapshot(
-    gg_playback()
+  playback_file <- file.path(tempdir(),"camcorder_playback.gif")
+  gg_playback(playback_file)
+
+  expect_snapshot_file(
+    path = playback_file
   )
 
 })
 
-test_that("recording a basic patchwork works", {
+test_that("resizing a plot works", {
 
-  withr::with_package("patchwork",code = {
-
-  rec_dir <- file.path(tempdir(),"camcorder_tests_patchwork")
+  rec_dir <- file.path(tempdir(),"camcorder_tests_ggplot")
 
   if(dir.exists(rec_dir)){
-    unlink(rec_dir)
+    unlink(rec_dir,recursive = TRUE)
   }
 
   gg_record(dir = rec_dir)
@@ -76,11 +153,7 @@ test_that("recording a basic patchwork works", {
   mtcars_plot <- ggplot2::ggplot(mtcars) +
     ggplot2::geom_boxplot(ggplot2::aes(x = factor(cyl), y = mpg))
 
-  mtcars_patchwork <- wrap_plots(mtcars_plot,mtcars_plot,ncol = 2)
-
-  expect_snapshot(
-    record_patchwork(mtcars_patchwork)
-  )
+  record_ggplot(mtcars_plot)
 
   ## Recording created directory
   expect_true(dir.exists(rec_dir))
@@ -89,44 +162,17 @@ test_that("recording a basic patchwork works", {
   expect_equal(length(list.files(rec_dir)), 1)
 
   ## preview film shows the file
-  expect_snapshot(
-    preview_film()
+  preview_html <-  preview_film()
+
+  file.rename(preview_html,file.path(tempdir(),"camcorder_preview_ggplot.html"))
+
+  expect_snapshot_file(
+    path = file.path(tempdir(),"camcorder_preview_ggplot.html")
   )
 
   expect_equal(
     GG_RECORDING_ENV$last_plot,
-    mtcars_patchwork
-  )
-
-  })
-})
-
-test_that("recording a basic graphic devices works", {
-
-  rec_dir <- file.path(tempdir(),"camcorder_tests_polaroid")
-
-  if(dir.exists(rec_dir)){
-    unlink(rec_dir)
-  }
-
-  gg_record(dir = rec_dir)
-  on.exit(gg_stop_recording())
-
-  plot(1:5)
-
-  expect_snapshot(
-    record_polaroid()
-  )
-
-  ## Recording created directory
-  expect_true(dir.exists(rec_dir))
-
-  ## Recording added a single file
-  expect_equal(length(list.files(rec_dir)), 1)
-
-  ## preview film shows the file
-  expect_snapshot(
-    preview_film()
+    mtcars_plot
   )
 
 })
